31e587e2a52277dc8d135468d68a55c8bcdc4aaa
kaila-6957 <kailaj@email.sc.edu>
2019-10-27 23:39:24 -0400

    first commit

---
+++ ProfilePage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ProfilePage());
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Profile Page')
+        ),
+        body: Center(
+            child: Column(
+                children: <Widget>[
+                  Text('Log out',
+                      style: TextStyle(color: Colors.black, fontSize: 20)),
+
+                  Text('Profile',
+                      style: TextStyle(color: Colors.purpleAccent,
+                          fontSize: 45,
+                          backgroundColor: Colors.grey)),
+
+                  SizedBox(height: 100.0),
+
+                  Text('ADD PROGRESS BAR'),
+
+                  SizedBox(height: 50.0),
+                  new Container(height: 1.5, color: Colors.black),
+
+                  Text('ADD NAME',
+                      style: TextStyle(color: Colors.purpleAccent, fontSize: 30)),
+                  new Container(height: 1.5, color: Colors.black),
+
+
+                ]
+            )
+        )
+    );
+  }
+}
